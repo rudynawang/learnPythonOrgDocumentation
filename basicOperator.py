cfec686808c25395373802d525fdaa8e0b99ca7a
Rudy Nawang <rudnawang07@gmail.com>
2025-11-08 00:06:31 +0545

    first commit

--- basicOperator.py
+++ basicOperator.py
@@ -1,14 +1,35 @@
-# Addition, Subtraction, Multiplication and Division operators used with numbers
+### Using Operators with Numbers
+#1. Addition, Subtraction, Multiplication and Division operators used with numbers
 number = 1 + 2 * 3 - 2 / 4.0
 print(number)
 
-# Another operator Modulo(%) operator
+#2. Another operator Modulo(%) operator
 remainder = 11 % 3
 print(remainder)
 
-# Two Multiplication Symbol makes Power/Exponentation
+#3. Two Multiplication Symbol makes Power/Exponentation
 squared = 4 ** 2
 cubed = 5 ** 3
 print(squared)
 print(cubed)
 
+### Using Operators with Strings
+#4. String Concatination done using Addition Operators
+mystring = "hello"+" "+"world"
+print(mystring)
+
+#5. Python also supports multiplying string with number to form string with a repeating sequence
+lotsOfString = "jellyFish " * 4
+print(lotsOfString)
+
+### Using Operators with List
+#6. List can be joined with addition operators
+evenNumbers = [2,4,6,8]
+oddNumbers = [1,3,5,7,9]
+allNumbers = evenNumbers + oddNumbers
+print(allNumbers)
+
+#7. Python supports forming new List with a repeating sequence using Multiplication operator
+oldList = [1,3,5]
+newList = oldList * 4
+print(newList)
