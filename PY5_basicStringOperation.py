18c73a88ef200f74476dd22424950e875900ea2f
Rudy Nawang <rudnawang07@gmail.com>
2025-11-18 00:45:39 +0545

    third commit

---
+++ PY5_basicStringOperation.py
@@ -0,0 +1,13 @@
+# Basic String Operation
+msg = "Don't over think"
+print(msg)
+
+# String Length
+str = "Hello world!"
+print(len(str))
+
+# character Position 
+astring = "Hello world!"
+print(astring.index('o'))
+
+
