feef8d419f99b92712fbfc1b09a895a98651d770
Rudy Nawang <rudnawang07@gmail.com>
2025-11-20 00:46:13 +0545

    fifth commit

--- PY5_basicStringOperation.py
+++ PY5_basicStringOperation.py
@@ -19,3 +19,15 @@ print(len(mylist[0]))
 # Counting the same character in the string
 print(mylist[3].count("r"))
 
+# Printing the slice of the string
+astring = "Hello world!"
+print(astring[3])     #prints only "l"
+print(astring[3:8])   #prints "lo wo"
+print(astring[:7])    #prints "Hello w"
+print(astring[2:])    #prints "llo world!"
+
+print(astring[-3])
+print(astring[-3:])
+print(astring[-1:-8])
+print(astring[-8:-3])
+
