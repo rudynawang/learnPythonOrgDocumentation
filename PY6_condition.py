98b7cdf24f3d23c8b947b8eb0216e7f2dcf9f5f5
Rudy Nawang <rudnawang07@gmail.com>
2025-11-21 16:21:20 +0545

    seventh commit

--- PY6_condition.py
+++ PY6_condition.py
@@ -1,9 +1,25 @@
+#1. Boolean Value/ Boolean Logic/ Boolean Logic Value -> {True, False}
 x=3
-print(x==3)
-print(x==5)
-print(x!=1)
-print(x<2)
-print(x>2)
-print(x<3)
-print(x>3)
+print(x==3) # x is equal to 3 (True)
+print(x==5) # x is equal to 5 (False)
+print(x!=1) # x is not equal to 1 (True)
+print(x<2)  # x is less than 2 (False)
+print(x>2)  # x is greater than 2 (True)
+print(x<3)  # x is less than 3 (False)
+print(x>3)  # x is greater than 3 (True)
+
+# Note: Variable assignment is done with single equal operator "=" where as comparision is done with double equals operator "=="
+
+#2. Boolean Operators -> {"and", "or"}
+name = "Ram"
+age = 23
+if name == "Ram" and age == 24:
+    print("value matched")
+else:
+    print("Value didn't matched") 
+
+if name == "Hari" or name == "Shyam":
+    print("Name is either 'Ram' or 'Hari'")
+else:
+    print("Name is neither 'Ram' nor 'Hari'")
 
