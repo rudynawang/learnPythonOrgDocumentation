b6a73690ece4c3b12fe58d34f8f3cd5a96ae41a0
Rudy Nawang <rudnawang07@gmail.com>
2026-01-29 23:52:44 +0545

    magh15 second

--- PY8_loop.py
+++ PY8_loop.py
@@ -1,13 +1,37 @@
-# Testing
-
+# For Loop
 nums= [9,8,7,6,5,4]
 for x in nums:
     print (x)
+
 print()
+
 for index, value in enumerate(nums):
     print(index, value)
 
 print()
 
+# For Loop using "range" and "xrange" function
+
 for i in range(5):
     print(i)
+
+print()
+
+for i in range(2,5): # having starting and ending value {range(start,end)}
+    print(i)
+
+print()
+
+for i in range(2,9,2): # having start, end and step value {range(start,end,step)}
+    print(i)
+
+print()
+
+# While Loop
+
+count = 0
+while count<7:
+    print(count)
+    count+=1
+
+    
