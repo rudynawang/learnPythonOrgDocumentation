3e581ca51b1728622d2cc8d6b4e0ea29bbd982db
Rudy Nawang <rudnawang07@gmail.com>
2026-01-31 00:16:55 +0545

    magh17 first

--- PY9_function.py
+++ PY9_function.py
@@ -11,3 +11,26 @@ def my_function_with_arguments(username,greetings):
 
 my_function_with_arguments("Rudra","Good Morning!")
 
+#3. Function may return a value to the caller, using the keyword- "return"
+def sum_two_numbers(a,b):
+    sum = a+b
+    return sum
+
+add = sum_two_numbers(5,6)
+print("The sum of two numbers is: %d" %(add))
+
+
+#4. Exercise
+
+def list_benefits():
+    return ["More organized code","More readable code","Easier code reuse","Allowing programmers to share and connect code together"]
+
+def build_sentence(benefit):
+    return (benefit+" is a benefit of functions!")
+
+def name_the_benefits_of_functions():
+    list_of_benefits= list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
