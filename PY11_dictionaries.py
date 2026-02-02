219cec28d165ad7ea832241e5e7d9bb15935dd2e
Rudy Nawang <rudnawang07@gmail.com>
2026-02-02 23:17:24 +0545

    magh19 first

--- PY11_dictionaries.py
+++ PY11_dictionaries.py
@@ -16,3 +16,36 @@ phonebook = {
 }
 
 print(phonebook)
+print()
+
+#3. Iterating over Dictionaries
+phonebook = {
+    "Rudra" : 9876543223,
+    "Krishna" : 9853478732,
+    "Maruti" : 9843258729
+}
+
+for name, phonenumber in phonebook.items():
+    print("Phone number of %s is %d" %(name, phonenumber))
+print()
+
+#4. Removing a value from the Dictionaries
+phonebook = {
+    "John" : 9876543223,
+    "Ram" : 9853478732,
+    "Hari" : 9843258729
+}
+
+del phonebook["John"]
+print(phonebook)
+print()
+
+#5. Next way to Remove the value/item from the Dictionaries
+phonebook = {
+    "John" : 9876543223,
+    "Ram" : 9853478732,
+    "Hari" : 9843258729
+}
+
+phonebook.pop("Hari")
+print(phonebook)
