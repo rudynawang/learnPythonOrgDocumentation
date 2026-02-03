136f526d05ca62c55675cddc51f226fcb9a3537a
Rudy Nawang <rudnawang07@gmail.com>
2026-02-03 23:20:07 +0545

    magh20 first

---
+++ PY13_inputandoutput.py
@@ -0,0 +1,22 @@
+#1. Input() Function
+print("Enter anything as input:")
+astr = input()
+print("You have typed:",astr)
+
+
+# converting input into our required datatypes
+
+num = int(input())
+print(num)
+print()
+
+flo = float(input())
+print(flo)
+print()
+
+st = str(input())
+print(st)
+print()
+
+
+print("ram", "Shyam", "hari")
