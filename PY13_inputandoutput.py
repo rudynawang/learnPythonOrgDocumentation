466edd84ff3f9deef8b2af869eb370821ccc41cd
Rudy Nawang <rudnawang07@gmail.com>
2026-02-04 00:29:55 +0545

    magh21 first

--- PY13_inputandoutput.py
+++ PY13_inputandoutput.py
@@ -4,7 +4,7 @@ astr = input()
 print("You have typed:",astr)
 
 
-# converting input into our required datatypes
+# converting input into our required datatypes using functions like int(),float(),str()
 
 num = int(float(input()))
 print(num)
@@ -19,4 +19,13 @@ print(st)
 print()
 
 
-print("ram", "Shyam", "hari")
+# how to take two or more data types as input from a single line separated by spaces?
+# Here we make use split() and map() functions
+
+a, b = map(int, input().split())
+array = input().split()
+add =0
+for each in array:
+    add = add +int(each)
+
+print(a, b, add)
