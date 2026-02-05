f225cdd4afde0eeff2a03536029af83db2bc2ab3
Rudy Nawang <rudnawang07@gmail.com>
2026-02-05 23:31:51 +0545

    magh22 second

--- PY12_modulespackages.py
+++ PY12_modulespackages.py
@@ -2,3 +2,16 @@
 # In python, modules are the python files with a ".py" extension.
 # Modules are imported 
 
+import draw
+print(draw.__file__)
+
+def play_game():
+    return "Game Started"
+
+def main():
+    result = play_game()
+    draw.draw_game(result)
+   
+
+if __name__ == '__main__':
+    main()
